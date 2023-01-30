4152c0ee6793fe3efca9637473540809fda901f6
mrdurbek <poyonovdurbek@gmail.com>
2023-01-30 15:43:11 +0500

    second commit

---
+++ src/main/java/pdp/uz/appclickup/entity/Attachment.java
@@ -0,0 +1,31 @@
+package pdp.uz.appclickup.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import pdp.uz.appclickup.entity.template.AbstractIntegerEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Attachment extends AbstractIntegerEntity {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String originalName;
+
+    @Column(nullable = false)
+    @Size(min = 1,max = 500)
+    private Long size;
+
+    @Column(nullable = false)
+    private String contentType;
+}
